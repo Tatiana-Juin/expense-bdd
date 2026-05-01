a9198bf6aded4af94879a59e23c6e83496b9d3e1
Tatiana-Juin <username@users.noreply.github.com>
2026-05-01 17:58:05 +0200

    J'ai créer chaque fonction dans expenseApi pour appeller mon api pour voir toutes les depense , ajout et suppression . C'est le chemin

--- frontend/expense-bdd/src/services/expenseApi.js
+++ frontend/expense-bdd/src/services/expenseApi.js
@@ -1,1 +1,33 @@
-const API_URL = import.meta.env.VITE_API_URL_DEV;
+const API_URL_DEV = import.meta.env.VITE_API_URL_DEV;
+
+// Pour afficher toute les depenses 
+export const getExpenses = async () => {
+  const res = await fetch(API_URL_DEV);
+  return await res.json();
+};
+// Pour creer une depense 
+export const createExpense = async (data) => {
+  const res = await fetch(API_URL_DEV, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  return await res.json();
+};
+// Pour supprimer une depense
+export const deleteExpense = async (id) => {
+  const res = await fetch(`${API_URL_DEV}/${id}`, {
+    method: "DELETE"
+  });
+
+  return await res.json();
+};
+
+// pour calculer le total 
+export const getTotal = async () => {
+  const res = await fetch(`${API_URL_DEV}/total`);
+  return await res.json();
+};
