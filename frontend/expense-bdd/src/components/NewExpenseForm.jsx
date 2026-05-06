49068bd3b3899dea04a7cf4324479384fa8742fb
Tatiana-Juin <username@users.noreply.github.com>
2026-05-06 19:25:19 +0200

    J'ai supprimer un commentaire inutile

--- frontend/expense-bdd/src/components/NewExpenseForm.jsx
+++ frontend/expense-bdd/src/components/NewExpenseForm.jsx
@@ -103,7 +103,7 @@ export default function NewExpenseForm({setDepenseEdit, onAddExpense,expenseToEd
               {/* BOUTON AJOUTER OU ANNULLER  */}
               <div className="flex justify-end space-x-4 pt-4">
 
-                  {/* <button type="submit"  className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 ease-in-out" >Ajouter depense</button> */}
+                  
                    <button className="w-full bg-blue-600 text-white p-2 rounded">
                       {expenseToEdit ? "Modifier" : "Ajouter une depense"}
                   </button>
